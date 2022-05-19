6131b5fdf8012b1e99c60daed73c8b9d3ecc82cf
David Schneemann <david.schneemann1@gmail.com>
2022-05-19 15:00:19 -0700

    add fct exercise work

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -10,3 +10,11 @@ WHERE last_name like 'e%e';
 SELECT upper(concat(first_name,' ', last_name)) as full_name
 FROM employees
 WHERE last_name like 'e%e';
+
+#4
+SELECT datediff(curdate(), hire_date) as tenure
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+
+#5

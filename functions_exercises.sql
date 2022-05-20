ff66d421567ac70e337746c9ae61c45eabd11813
David Schneemann <david.schneemann1@gmail.com>
2022-05-20 07:28:57 -0700

    morning update

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,6 +15,21 @@ WHERE last_name like 'e%e';
 SELECT datediff(curdate(), hire_date) as tenure
 FROM employees
 WHERE hire_date LIKE '199%'
-AND birth_date LIKE '%-12-25'
+AND birth_date LIKE '%-12-25';
 
 #5
+select * from salaries;
+
+select max(salary) from salaries;
+select min(salary) from salaries;
+
+# max = $158220
+# min = $38623
+
+#6
+select * from employees;
+SELECT lower(concat(substr(first_name,1,1), substr(last_name,1,4), '_', substr(birth_date,6,2), substr(birth_date,3,2))) as username
+FROM employees;
+
+# yay :D
+

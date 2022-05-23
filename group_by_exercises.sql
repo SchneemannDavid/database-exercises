ab6c268b8c99e1559289ec857b5f022ceeda9bbb
David Schneemann <david.schneemann1@gmail.com>
2022-05-23 13:30:17 -0700

    add subqueries_exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,14 +1,77 @@
-select first_name, count(DISTINCT(first_name))
-from employees
-WHERE first_name NOT LIKE '%a%'
-GROUP BY first_name;
+use employees;
 
-SELECT last_name, count(*) AS n_same_last_name
-FROM employees
-GROUP BY last_name
-HAVING n_same_last_name < 150;
+select * from titles;
 
-SELECT concat(first_name, " ", last_name) AS full_name, count(*) AS n_same_full_name
+#2
+SELECT title, COUNT(DISTINCT(title))
+FROM titles
+GROUP BY title;
+# how many distinct titles: 6
+
+#3
+SELECT emp_no, last_name, COUNT(last_name)
 FROM employees
-GROUP BY full_name
-HAVING n_same_full_name >= 5;
+WHERE last_name LIKE 'e%e'
+GROUP BY emp_no, last_name;
+
+#4
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name;
+
+#5
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+# last_names: Chleq, Lindqvist, Qiwen
+
+#6
+SELECT last_name, first_name, count(*)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name, first_name;
+
+#7
+SELECT gender, first_name, COUNT(*)
+FROM employees
+WHERE first_name = 'Irena'
+	or first_name = 'Vidya'
+    or first_name = 'Maya'
+GROUP BY gender, first_name;
+### easier method:
+SELECT gender, first_name, COUNT(gender)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender, first_name;
+
+#8
+SELECT lower(concat(substr(first_name,1,1), 
+substr(last_name,1,4), 
+'_', 
+substr(birth_date,6,2), 
+substr(birth_date,3,2))) 
+as username,
+COUNT(*) as number_with_unique_username
+FROM employees
+GROUP BY username;
+
+# to find number of duplicate usernames:
+SELECT lower(concat(substr(first_name,1,1), 
+substr(last_name,1,4), 
+'_', 
+substr(birth_date,6,2), 
+substr(birth_date,3,2))) 
+as username,
+COUNT(*) as number_with_unique_username
+FROM employees
+GROUP BY username
+HAVING number_with_unique_username >1;
+# duplicates: 13251
+
+#9. BONUS
+
+
+
+
---
+++ subqueries_exercises.sql
@@ -0,0 +1,166 @@
+
+#1
+SELECT hire_date
+FROM employees
+WHERE emp_no = '101010';
+
+SELECT first_name, last_name
+FROM employees as e
+JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+WHERE hire_date = (SELECT hire_date
+FROM employees WHERE emp_no = '101010')
+AND de.to_date > NOW();
+
+
+
+#2
+SELECT emp_no
+FROM employees
+WHERE first_name = 'Aamod';
+
+SELECT DISTINCT(title), first_name, last_name
+FROM titles
+JOIN dept_emp
+	USING (emp_no)
+JOIN employees	
+	USING (emp_no)
+WHERE emp_no IN (SELECT emp_no
+FROM employees
+WHERE first_name = 'Aamod') AND dept_emp.to_date > NOW();
+
+
+#3
+SELECT emp_no
+FROM dept_emp 
+WHERE to_date < CURDATE();
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (SELECT emp_no
+FROM dept_emp 
+WHERE to_date < CURDATE());
+
+
+#4
+Select emp_no
+FROM dept_manager
+WHERE to_date > NOW();
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Department Managers'
+FROM employees
+WHERE emp_no IN 
+	(Select emp_no
+	FROM dept_manager
+	WHERE to_date > NOW())
+AND gender = 'F';
+
+-- Isamu Legleitner
+-- Karsten Sigstam
+-- Leon DasSarma
+-- Hilary Kambil
+
+#5
+SELECT AVG(salary)
+FROM salaries;
+
+SELECT first_name, last_name
+FROM employees
+JOIN salaries USING (emp_no)
+WHERE to_date > NOW() AND salary > (SELECT AVG(salary)
+FROM salaries);
+
+
+#6
+SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW();
+-- 158220
+
+SELECT std(salary)
+FROM salaries
+WHERE to_date > NOW();
+-- 17309.9593
+
+SELECT COUNT(*)
+FROM salaries
+WHERE salary >= (SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW()) - (SELECT std(salary)
+FROM salaries
+WHERE to_date > NOW())
+AND to_date > NOW();
+-- 83 current salaries
+
+SELECT (83 / COUNT(*))*100
+FROM salaries
+WHERE to_date > NOW();
+-- Percentage of all salaries = .03%
+
+
+-- ----BONUS----
+## Find all department names that currently have female managers
+
+-- copied down from Q4
+Select emp_no
+FROM dept_manager
+WHERE to_date > NOW();
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Department Managers'
+FROM employees
+WHERE emp_no IN 
+	(Select emp_no
+	FROM dept_manager
+	WHERE to_date > NOW())
+AND gender = 'F';
+
+## B1
+SELECT dept_name AS 'Departments'
+FROM departments
+JOIN dept_emp USING (dept_no)
+WHERE emp_no 
+IN (SELECT emp_no
+	FROM employees
+	WHERE emp_no IN 
+		(Select emp_no
+		FROM dept_manager
+		WHERE to_date > NOW())
+	AND gender = 'F');
+    
+
+## B2
+SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW();
+
+SELECT emp_no
+FROM salaries
+WHERE salary = (SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW());
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE emp_no = (SELECT emp_no
+FROM salaries
+WHERE salary = (SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW()));
+-- Tokuyasu Pesch
+
+## B3
+SELECT dept_name AS 'Department Name'
+FROM departments
+JOIN dept_emp USING (dept_no)
+WHERE emp_no = (SELECT emp_no
+FROM salaries
+WHERE salary = (SELECT MAX(salary)
+FROM salaries
+WHERE to_date > NOW()));
+
+
+
+
+
+
+
